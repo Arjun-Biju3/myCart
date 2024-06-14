391d14fa8a2372e33755220c83aa8a5d678c7ae3
Arjun Biju <arjunbiju332@gmail.com>
2024-06-14 23:03:27 +0530

    Data model design complted with modification and primary template design completed

--- products/urls.py
+++ products/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('products.urls')),
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('',views.index,name='home'),
+     path('products/',views.list_products,name='productList'),
+     path('detailed/',views.detail_product,name='detailedProduct'),
+     path('cart/',views.cart,name='cart'),
+     path('account/',views.account,name='account')
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def list_products(request):
+    return render(request,'product_layout.html')
+
+def detail_product(request):
+    return render(request,'product_details.html')
+
+def cart(request):
+    return render(request,'cart.html')
+
+def account(request):
+    return render(request,'account.html')
+
