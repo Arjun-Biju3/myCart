837a17b963a85ae73039e0c73d94787d886e4aa3
Arjun Biju <arjunbiju332@gmail.com>
2024-06-18 12:25:07 +0530

    oder partial stage

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .  models import Order,OrderedItem 
+from products.models import Product
 
 def cart(request):
     return render(request,'cart.html')
+
+def add_to_cart(request):
+    if request.POST:
+        user=request.user
+        customer=user.customer_profile
+        quantity=int(request.POST.get('quantity'))
+        product_id=request.POST.get('product_id')
+        cart_obj,created=Order.objects.get_or_create(
+            owner=customer,
+            order_status=Order.CART_STAGE
+        )
+        product=Product.objects.get(pk=product_id)
+        ordered_item=OrderedItem.objects.create(
+            product=product,
+            owner=cart_obj,
+            quantity=quantity
+        )
+        return redirect('cart')
+        
