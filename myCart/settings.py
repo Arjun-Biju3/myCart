a9e48c48909afdf89b23e0137f326986be4f2c7f
Arjun Biju <arjunbiju332@gmail.com>
2024-06-24 16:13:29 +0530

    requirements file added

--- myCart/settings.py
+++ myCart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bw68@4ztk$jv=w16%nu)_np=paq78#)q_074erl2b&%(sj^m$x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
